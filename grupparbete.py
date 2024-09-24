e2a9400b8dc8b77e25b0bc353c895005edfdbab6
Jing Z <zhang.jing.1997@outlook.com>
2024-09-24 15:28:18 +0200

    Add files via upload

---
+++ grupparbete.py
@@ -0,0 +1,12 @@
+#importera allt som behövs
+
+#skapa alla inputs användaren kommer svara på
+
+#läs från csv filen och sortera utifrån användarens inputs
+#med hjälp av loopar
+
+#printa ut 3-5 filmer efter sorteringen
+
+val = ["drama", "comedy"]
+import pyinputplus  as pyip
+svar = pyip.inputMenu(val, prompt=  "Whatcha want?" )
