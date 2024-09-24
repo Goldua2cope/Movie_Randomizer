55c8c4885f3f6a0344263f595f0f3d534955b21c
Jing Z <zhang.jing.1997@outlook.com>
2024-09-24 15:49:00 +0200

    Update grupparbete.py

--- grupparbete.py
+++ grupparbete.py
@@ -1,6 +1,8 @@
 #importera allt som behövs
+import pyinputplus as pyip
 
 #skapa alla inputs användaren kommer svara på
+tid = pyip.inputMenu(choices = ['1-2 h', '2-3 h'], prompt= 'How much time do you have?\n', numbered=True)
 
 #läs från csv filen och sortera utifrån användarens inputs
 #med hjälp av loopar
