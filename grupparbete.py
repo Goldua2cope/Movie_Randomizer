8680ecefdcf462774d961d40f1157861487f67ce
Jing Z <zhang.jing.1997@outlook.com>
2024-09-24 17:27:22 +0200

    Update grupparbete.py

--- grupparbete.py
+++ grupparbete.py
@@ -4,6 +4,15 @@ import pyinputplus as pyip
 #skapa alla inputs användaren kommer svara på
 tid = pyip.inputMenu(choices = ['1-2 h', '2-3 h'], prompt= 'How much time do you have?\n', numbered=True)
 
+def time_in_minutes(hours: str) -> int: #returnerar max timmar till minuter
+    match hours:
+        case '1-2 h':
+            return 120
+        case '2-3 h':
+            return 180
+        case _:
+            raise ValueError('ValueError!') #
+          
 #läs från csv filen och sortera utifrån användarens inputs
 #med hjälp av loopar
 
