cb5b1cfbeaf0793ff8fc0c36ff0d72e5edf332ba
Jing Z <zhang.jing.1997@outlook.com>
2024-10-02 23:51:12 +0200

    pretty

--- grupparbete.py
+++ grupparbete.py
@@ -10,7 +10,7 @@ df = pd.read_csv('movies.csv')
 class Movies:
     def __init__(self, original_title = None ,year = None, genre = None, duration = None, director = None) -> None:
         # Initiate attributes for a movie object 
-        self.original_title: str = original_title 
+        self.original_title : str = original_title 
         self.year : int = year
         self.genre : str = genre
         self.duration : int = duration
@@ -26,22 +26,22 @@ class Movies:
 # Class that filters movies from 'df'
 class Movies_Filter:
     # Initiate data frame
-    def __init__(self, df: pd.DataFrame): 
+    def __init__(self, df : pd.DataFrame): 
         self.df = df
     
     # methods for filtering data frame according to user input, return new data frames
     def filter_duration(self, time_limit : int) -> pd.DataFrame:
         return self.df[self.df['duration'] <= time_limit] 
     
-    def filter_year(self,df, year_range : tuple) -> pd.DataFrame:
+    def filter_year(self, df : pd.DataFrame, year_range : tuple) -> pd.DataFrame:
         return df[(df['year'] >= year_range[0]) & (df['year'] <= year_range[1])] 
     
-    def filter_genre(self, df , genre_choice : str) -> pd.DataFrame:
+    def filter_genre(self, df : pd.DataFrame, genre_choice : str) -> pd.DataFrame:
         if genre_choice != 'Does not matter': 
             return df[df['genre'].str.contains(genre_choice, case=False, na=False)]  
         return df 
     
-    def filter_director(self, df, director_choice : str) -> pd.DataFrame:
+    def filter_director(self, df : pd.DataFrame, director_choice : str) -> pd.DataFrame:
         if director_choice != 'Does not matter': 
              return df[df['director'] == director_choice]
         return df
@@ -101,49 +101,49 @@ class Movie_program:
     def main_program_runs(self) -> None:
         # Prompting for User preference
         # pyip.inputXX handels ValueErrors and prompts user until a valid input is given
-        time_choice = pyip.inputMenu(choices=['1-2 h', '2-3 h', 'Does not matter'], 
-                             prompt='How much time do you have?\n',
-                             numbered=True,
+        time_choice = pyip.inputMenu(choices = ['1-2 h', '2-3 h', 'Does not matter'], 
+                             prompt = 'How much time do you have?\n',
+                             numbered = True,
                              postValidateApplyFunc = self.time_in_minutes)
         
-        genre_choice_input = pyip.inputMenu(choices = self.make_genre_list(), prompt='Which genre do you prefer?\n', numbered=True)
+        genre_choice_input = pyip.inputMenu(choices = self.make_genre_list(), prompt = 'Which genre do you prefer?\n', numbered = True)
         
-        year_choice = pyip.inputMenu(choices=['Before 2000', 'After 2000', 'Does not matter'],
-                             prompt='You wanna see an older or newer movie?\n', 
-                             numbered=True,
+        year_choice = pyip.inputMenu(choices = ['Before 2000', 'After 2000', 'Does not matter'],
+                             prompt = 'You wanna see an older or newer movie?\n', 
+                             numbered = True,
                              postValidateApplyFunc = self.year_filter)
     
-        director_choice = pyip.inputMenu(choices = self.make_director_list(), prompt='Do you have any favorite director?\n', numbered=True)
+        director_choice = pyip.inputMenu(choices = self.make_director_list(), prompt = 'Do you have any favorite director?\n', numbered = True)
 
         # Filtering data frame based on user preference
-        filtered_df = self.movies_filter.filter_duration(time_choice)
-        filtered_df = self.movies_filter.filter_year(filtered_df, year_choice)
-        filtered_df = self.movies_filter.filter_genre(filtered_df, genre_choice_input)
-        filtered_df = self.movies_filter.filter_director(filtered_df,director_choice)
+        filtered_df : pd.DataFrame = self.movies_filter.filter_duration(time_choice)
+        filtered_df : pd.DataFrame = self.movies_filter.filter_year(filtered_df, year_choice)
+        filtered_df : pd.DataFrame = self.movies_filter.filter_genre(filtered_df, genre_choice_input)
+        filtered_df : pd.DataFrame = self.movies_filter.filter_director(filtered_df,director_choice)
 
         # Display 3 random movies based on the filtered data frame
         if not filtered_df.empty:
             num_movies = min(3, len(filtered_df))  
-            selected_movies = filtered_df.sample(n=num_movies) 
+            selected_movies = filtered_df.sample(n = num_movies) 
             for index, movie_row in selected_movies.iterrows():
                 movie = Movies(
-                    original_title=movie_row['original_title'], 
-                    year=movie_row['year'], 
-                    genre=movie_row['genre'], 
-                    duration=movie_row['duration'], 
-                    director=movie_row['director']
+                    original_title = movie_row['original_title'], 
+                    year = movie_row['year'], 
+                    genre = movie_row['genre'], 
+                    duration = movie_row['duration'], 
+                    director = movie_row['director']
                )
                 print(f"Movies matching your criteria:\n{movie}")
         else:
             print("Cannot find a film for you today.")
             
             # Extra option in case no matches are found, prints 3 random movies of all movies
-            # pyip.inputYesNo handels ValueErrors and prompts user until a valid input is given
-            lucky_choice = pyip.inputYesNo(prompt='Would you like to watch a random Movie of our choice? (yes/no)\n')
+            # pyip.inputYesNo handles ValueErrors and prompts user until a valid input is given
+            lucky_choice = pyip.inputYesNo(prompt = 'Would you like to watch a random Movie of our choice? (yes/no)\n')
 
             if lucky_choice == 'yes':
                 num_movies = min(3, len(self.df))
-                selected_movies = self.df.sample(n= num_movies)
+                selected_movies = self.df.sample(n = num_movies)
 
                 for index, movie_row in selected_movies.iterrows():
                     print(f"\nTitle: {movie_row['original_title']}")
